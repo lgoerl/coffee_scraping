c4ee1c714ce938a8e74b279ad5ccc977527006af
lee <lgoerl@gmail.com>
2022-01-16 20:36:17 -0800

    configure db output op correctly

--- src/dags/ops/product_scraping.py
+++ src/dags/ops/product_scraping.py
@@ -1,8 +1,8 @@
 from pandas import DataFrame
 from typing import List
 
-from dagster import op, DynamicOut
-from dags.resources.api_client.base_api_scraper import return_columns
+from dagster import DynamicOut, DynamicOutput, Out, op
+from dags.resources.api_clients.base_api_scraper import return_columns
 
 @op(
     required_resource_keys={"api_client"},
@@ -11,15 +11,18 @@ from dags.resources.api_client.base_api_scraper import return_columns
 def get_product_list(context):
     products = context.resources.api_client.get_active_roasts()
     for product in products:
-        yield DynamicOut(
-            value=product,
-            mapping_key=product.replace("/", "_"),
+        yield DynamicOutput(
+            product,
+            product.replace("/", "_").replace("-", "_"),
         )
 
 @op(required_resource_keys={"api_client"})
 def get_product_info(context, product: str):
     return context.resources.api_client.get_roast(product)
 
-@op()
+@op(out=Out(
+    io_manager_key="warehouse_io_manager",
+    metadata={"table": "dbt.stg_coffee_meta", "partitioned": False}
+))
 def process_product_collection(products: List[dict]) -> DataFrame:
     return DataFrame.from_records(products)[return_columns]
